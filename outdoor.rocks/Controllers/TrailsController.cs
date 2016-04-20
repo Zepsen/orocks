7f8552c2bb398bab23eb582cba1ee5ded30f97d4
zepsen <zepsendm@gmail.com>
2016-04-20 17:03:39 +0300

    get and getbyid from db

---
+++ outdoor.rocks/Controllers/TrailsController.cs
@@ -0,0 +1,52 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using outdoor.rocks.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace outdoor.rocks.Controllers
+{
+    public class TrailsController : ApiController
+    {
+        private DbContext db = new DbContext();
+
+        // GET: api/Trails
+        //Get All trails
+        public IEnumerable<string> Get()
+        {
+            var collection = db.Context.GetCollection<Trails>("Trails");
+            var filter = new BsonDocument();
+            var seasons = collection.Find(filter).ToList();
+            var res = seasons.Select(i=>i.ToJson());
+            return res;
+        }
+
+        // GET: api/Trails/5
+        public string Get(string id)
+        {      
+            var collection = db.Context.GetCollection<Trails>("Trails");
+            var season = collection.Find(i => i._id == ObjectId.Parse(id)).ToList<Trails>();
+            return season.FirstOrDefault().ToJson();
+        }
+
+        // POST: api/Trails
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Trails/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Trails/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- outdoor.rocks/Models/Models.cs
+++ outdoor.rocks/Models/Models.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,31 +7,31 @@ using System.Web;
 
 namespace outdoor.rocks.Models
 {
-    public class Trail
+    public class Trails
     {
         public ObjectId _id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string WhyGo { get; set; }
-        public Difficults Difficult_Id { get; set; }
-        public Locations Location_Id { get; set; }
-        public Options Option_Id { get; set; }
-        public List<Comments> Comments { get; set; }
-        public References Reference_Id { get; set; }
+        public ObjectId Difficult_Id { get; set; }
+        public ObjectId Location_Id { get; set; }
+        public ObjectId Option_Id { get; set; }
+        public List<ObjectId> Comments_Ids { get; set; }
+        public ObjectId Reference_Id { get; set; }
     }
 
     public class Difficults
-    {
+    {        
         public ObjectId _id { get; set; }
         public string Value { get; set; }
     }
 
     public class Locations
     {
-        public ObjectId _Id { get; set; }
-        public Regions Region_Id { get; set; }
-        public Countries Country_Id { get; set; }
-        public States State_Id { get; set; }
+        public ObjectId _id { get; set; }
+        public ObjectId Region_Id { get; set; }
+        public ObjectId Country_Id { get; set; }
+        public ObjectId State_Id { get; set; }
     }
 
     public class Regions
@@ -43,7 +44,7 @@ namespace outdoor.rocks.Models
     {
         public ObjectId _id { get; set; }
         public string Name { get; set; }
-        public Regions Region_Id { get; set; }
+        public ObjectId Region_Id { get; set; }
     }
 
     public class States
@@ -56,7 +57,7 @@ namespace outdoor.rocks.Models
     public class Comments
     {
         public ObjectId _id { get; set; }
-        public Users User_Id { get; set; }
+        public ObjectId User_Id { get; set; }
         public string Comment { get; set; }
         public int Rate { get; set; }
 
@@ -70,10 +71,10 @@ namespace outdoor.rocks.Models
         public int Peak { get; set; }
         public bool DogAllowed { get; set; }
         public bool GoodForKids { get; set; }
-        public TrailsTypes TrailType_Id { get; set; }
-        public TrailsDurationTypes TrailDurationType_Id { get; set; }
-        public Seasons SeasonStart_Id { get; set; }
-        public Seasons SeasonEnd_Id { get; set; }
+        public ObjectId TrailType_Id { get; set; }
+        public ObjectId TrailDurationType_Id { get; set; }
+        public ObjectId SeasonStart_Id { get; set; }
+        public ObjectId SeasonEnd_Id { get; set; }
     }
 
     public class Seasons
@@ -105,7 +106,7 @@ namespace outdoor.rocks.Models
         public ObjectId _id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public Roles Role_Id { get; set; }
+        public ObjectId Role_Id { get; set; }
     }
 
     public class Roles
