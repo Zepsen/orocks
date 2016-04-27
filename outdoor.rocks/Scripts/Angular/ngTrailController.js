4a11a8557c7136be095fb2bb657a894a15ae5ff9
zepsen <zepsendm@gmail.com>
2016-04-27 13:36:34 +0300

    trail-getjson

--- outdoor.rocks/Scripts/Angular/ngTrailController.js
+++ outdoor.rocks/Scripts/Angular/ngTrailController.js
@@ -9,9 +9,14 @@ appTrail.controller("TrailCtrl", function ($scope, $http) {
         url: "../../api/Trails/" + $scope.trailId
     })
    .success(function (response) {
-
+       $scope.trail = JSON.parse(response);
+       var name = $scope.trail.Name;
+       $scope.trailComments = []
+       for(var i=0; i < $scope.trail.Comments.length; i++) {
+           $scope.trailComments.push(JSON.parse($scope.trail.Comments[i]));
+       }
+       
    });
-
 
 
 });
