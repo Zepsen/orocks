a812f2f95980d8761e6958d6a928762fdbc368e5
zepsen <zepsendm@gmail.com>
2016-05-16 15:33:58 +0300

    first test to trail controller

--- outdoor.rocks.Tests/UnitTest1.cs
+++ outdoor.rocks.Tests/UnitTest1.cs
@@ -1,12 +1,24 @@
 ﻿using outdoor.rocks.Controllers;
+using outdoor.rocks.Models;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Web.Mvc;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace outdoor.rocks.Tests
 {    
     public class UnitTest1
     {
+        private readonly ITestOutputHelper output;
+
+        public UnitTest1(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+
         [Fact]
         public void TestMethod1()
         {
@@ -30,7 +42,38 @@ namespace outdoor.rocks.Tests
             var test = await ctrl.Get();
 
             //Assert
-            Assert.True(test.Count > 0);
+            Assert.NotEmpty(test);
+            output.WriteLine("Trails get count = " + test.Count);                   
+        }
+
+        [Fact]
+        public async void TrailsGetReturnListTrailModelTest()
+        {
+            //Arrange
+            var ctrl = new TrailsController();
+
+            //Act 
+            var test = await ctrl.Get();
+
+            //Assertr
+            Assert.True(test is List<TrailModel>);
+            output.WriteLine("Trails get return List<TrailModel>");
+        }
+
+        [Fact]
+        public async void TrailsGetReturnCorrectDataTest()
+        {
+            //Arrange
+            var ctrl = new TrailsController();            
+
+            //Act 
+            var test = await ctrl.Get();
+            var trail = test[0];
+            //Assertr
+            Assert.NotNull(trail.Id);
+            Assert.NotNull(trail.Name);
+            Assert.True(trail.DogAllowed);
+            output.WriteLine("Trails return correct data");
         }
     } 
 
