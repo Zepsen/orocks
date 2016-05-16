1659a3176fef488c5e9dbc1e85e0749c602e20c2
zepsen <zepsendm@gmail.com>
2016-05-16 13:09:37 +0300

    add xunit test projects

---
+++ outdoor.rocks.Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using outdoor.rocks.Controllers;
+using System;
+using System.Web.Mvc;
+using Xunit;
+
+namespace outdoor.rocks.Tests
+{    
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            //Arrange
+            var ctrl = new HomeController();
+
+            //Act 
+            var test = ctrl.Index() as ViewResult;
+
+            //Assert
+            Assert.True(test.ViewBag.Title == "OUTDOOR.ROCKS");            
+        }
+
+        [Fact]
+        public async void TrailsGetTest()
+        {
+            //Arrange
+            var ctrl = new TrailsController();
+
+            //Act 
+            var test = await ctrl.Get();
+
+            //Assert
+            Assert.True(test.Count > 0);
+        }
+    } 
+
+}
