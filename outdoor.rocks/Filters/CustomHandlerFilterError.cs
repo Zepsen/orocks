f2d7fb5ed9e0ca27248f9a8f47ed0c11ecd940a6
zepsen <zepsendm@gmail.com>
2016-06-09 11:52:52 +0300

    add log to untreated exception

--- outdoor.rocks/Filters/CustomHandlerFilterError.cs
+++ outdoor.rocks/Filters/CustomHandlerFilterError.cs
@@ -8,40 +8,46 @@ namespace outdoor.rocks.Filters
 {
     public class CustomHandlerFilterError : ExceptionFilterAttribute
     {
-        private static  Logger log = LogManager.GetCurrentClassLogger();
+        private readonly static Logger Log = LogManager.GetCurrentClassLogger();
 
         public override void OnException(HttpActionExecutedContext context)
         {
             if (context.Exception is IdFormatException)
             {
-                CreateErrorResponse(context, 400);
+                context.Response = CreateErrorResponse(context, 400);
             }
 
             if (context.Exception is NotFoundException)
             {
-                CreateErrorResponse(context, 404);
+                context.Response = CreateErrorResponse(context, 404);
             }
 
             if (context.Exception is NotFoundByIdException)
             {
-                CreateErrorResponse(context, 429);
+                context.Response = CreateErrorResponse(context, 429);
             }
 
             if (context.Exception is ServerConnectionException)
             {
-                CreateErrorResponse(context, 430);
+                context.Response = CreateErrorResponse(context, 430);
             }
             
             if (context.Exception is NotImplementedException)
             {
-                CreateErrorResponse(context, 501);
+                context.Response = CreateErrorResponse(context, 501);
             }
+
+            if (context.Response == null)
+            {
+                Log.Error($"Untreated Exception {context.Exception.Message}");
+            }
+
         }
 
-        private static void CreateErrorResponse(HttpActionExecutedContext context, int status)
+        private static HttpResponseMessage CreateErrorResponse(HttpActionExecutedContext context, int status)
         {
-            log.Info($"Exception with status {status}");
-            context.Response = context.Request.CreateErrorResponse(
+            Log.Debug($"Exception with status {status}");
+            return context.Request.CreateErrorResponse(
                 (HttpStatusCode)status,
                 context.Exception.Message,
                 context.Exception);
